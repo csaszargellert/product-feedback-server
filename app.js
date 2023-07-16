131ddcf4645dcdc2778e9d7427f74dcb402caf54
Gellért Császár <csaszargellert@gmail.com>
2023-07-16 09:30:13 +0200

    rewrite allowed origin

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors({ origin: "http://127.0.0.1:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://gellert-product-feedback.netlify.app",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
